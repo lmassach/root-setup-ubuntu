fc100ea853c782bfb6c827f7a9bdabf75ff9d85d
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2022-05-01 21:58:36 +0200

    Imported the latest version of the script.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# root-setup-ubuntu
-Automatic downloader/compiler/installer script for ROOT on Ubuntu-based distributions.
+# ROOT setup for Ubuntu
+Automatic downloader/compiler/installer script for [ROOT](https://root.cern.ch/) on Ubuntu-based distributions, distributed under the GNU GPL v3.
+
+## How to use
+Download and launch the `root_setup.py` file:
+```bash
+wget https://github.com/lmassach/root-setup-ubuntu/raw/main/root_setup.py
+chmod +x root_setup.py
+./root_setup.py
+```
+This will:
+ - create a `root` directory in your home (i.e. `~/root`);
+ - copy the script in it;
+ - clone the [ROOT repo](https://github.com/root-project/root/) in `~/root/root` and switch to the `latest-stable` branch;
+ - install the dependencies (this will run `sudo apt -y install [...]`: you may be asked for yor password);
+ - configure ROOT to be built in `~/root/build` and installed in `~/root/install`;
+ - compile ROOT - this usually takes order of an hour and will use all the available CPU cores.
+
+For more options, see `./root_setup.py --help`.
+
+## Extra: opening ROOT files from the file explorer
+How to setup the `fileopen` macro to be used by the desktop shell (tested for GNOME):
+ - create a shell script `~/root/do_fileopen.sh` with the following content and make it executable;
+   ```bash
+   #!/bin/bash
+   source ~/root/install/bin/thisroot.sh
+   root "$1" ~/root/install/macros/fileopen.C
+   ```
+ - create a desktop file `~/.local/share/applications/root_fileopen.desktop` with the following content;
+   ```
+   [Desktop Entry]
+   Comment=
+   Terminal=true
+   Name=root_fileopen
+   Exec=~/root/do_fileopen.sh %f
+   Type=Application
+   Icon=~/root/install/icons/Root6Icon.png
+   Encoding=UTF-8
+   Hidden=false
+   NoDisplay=false
+   ```
+ - try to open a `.root` file by double-clicking, and select to use _root_fileopen_ to open it.
+
+In the above steps, you might have to replace `~` with the absolute path to your home.
+You might also want to add the `--web=off` to the ROOT command if you want the old `TBrowser` window to be used (instead of the new web-based browser introduced in ROOT 6.26).
---
+++ root_setup.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""
+ROOT setup script for Debian-based distributions.
+
+The script clones the ROOT git repository (latest-stable branch) and
+compiles ROOT.
+"""
+import os
+import sys
+import re
+import argparse
+import subprocess
+import shutil
+
+SCRIPT_PATH = os.path.abspath(__file__)
+SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
+IS_LOCAL_INSTALL = os.path.isfile(os.path.join(SCRIPT_DIR, ".root_setup_py"))
+DEPENDENCIES = [
+    "dpkg-dev", "cmake", "g++", "gcc", "binutils", "libx11-dev", "libxpm-dev",
+    "libxft-dev", "libxext-dev", "python3", "libssl-dev", "gfortran",
+    "libpcre3-dev", "libglu1-mesa-dev", "libglew-dev", "libftgl-dev",
+    "libmysqlclient-dev", "libfftw3-dev", "libcfitsio-dev", "libgraphviz-dev",
+    "libavahi-compat-libdnssd-dev", "libldap2-dev", "python3-dev",
+    "libxml2-dev", "libkrb5-dev", "libgsl-dev"
+]
+CMAKE_FLAGS = ["-DCMAKE_CXX_STANDARD=17"]
+
+
+def run(*args, check=True, cwd=None, timeout=None, capture=False):
+    cp = subprocess.run(
+        args, check=check, cwd=cwd, timeout=timeout,
+        stdout=(subprocess.PIPE if capture else None),
+        encoding=("utf8" if capture else None))
+    if capture:
+        return cp.stdout
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-d", "--install-dir", type=os.path.abspath,
+                        default=(SCRIPT_DIR if IS_LOCAL_INSTALL
+                                 else os.path.join(os.environ['HOME'], "root")),
+                        help="Installation directory. Default is $HOME/root,"
+                              " or the script's directory if an installation"
+                              " is already present there.")
+    parser.add_argument("-b", "--branch", default="latest-stable",
+                        help="Branch of the ROOT git repo to use (default"
+                             " latest-stable).")
+    parser.add_argument("-j", type=int, default=min(8, os.cpu_count()),
+                        help="Number of concurrent processes (default is the"
+                        " number of processors, but no more than 8).")
+    parser.add_argument("--clean", action="store_true",
+                        help="Remove old build and install directories before building.")
+    args = parser.parse_args()
+
+    print(f"Cd'ing into install directory {args.install_dir}")
+    os.makedirs(args.install_dir, exist_ok=True)
+    os.chdir(args.install_dir)
+
+    if os.path.isdir("root"):
+        if not os.path.isdir("root/.git"):
+            print("FATAL Found a root directory, but it is not a git repo.")
+            sys.exit(1)
+        print("Previous installation found")
+        if not os.path.isfile("root_setup.py") \
+            or not os.path.samefile("root_setup.py", SCRIPT_PATH):
+            print("Updating local copy of root_setup.py.")
+            shutil.copy(SCRIPT_PATH, "./root_setup.py")
+        print("Updating repo")
+        run("git", "fetch", cwd="./root")
+        run("git", "checkout", args.branch, cwd="./root")
+        try:
+            run("git", "pull", cwd="./root")
+        except subprocess.CalledProcessError:
+            print("Could not git-pull. This may happen if github is unreachable")
+            print("or if the branch given with -b is a tag.")
+            print("Trying to compile anyway.")
+    else:
+        print("No previous installation found, making a new one")
+        shutil.copy(SCRIPT_PATH, "./root_setup.py")
+        run("git", "clone", "--branch", args.branch,
+            "https://github.com/root-project/root.git")
+
+    print("Preparing build")
+    if args.clean:
+        if os.path.isdir("./build"):
+            print("Clean: removing old build directory...")
+            shutil.rmtree("./build")
+        if os.path.isdir("./install"):
+            print("Clean: removing old install directory...")
+            shutil.rmtree("./install")
+    os.makedirs("./build", exist_ok=True)
+    os.makedirs("./install", exist_ok=True)
+
+    print("Checking dependencies")
+    packages = run("apt", "list", "--installed", capture=True)
+    packages = [m[1] for m in re.finditer(r"(?:^|\n)(.+?)\/", packages)]
+    missing = [x for x in DEPENDENCIES if x not in packages]
+    if missing:
+        print(f"Installing missing dependencies: {' '.join(missing)}")
+        print("You may want to run an update/full-upgrade first")
+        run("sudo", "apt", "-y", "install", *missing)
+
+    print("Checking if additional CMAKE flags are needed")
+    packages = run("apt", "list", "--installed", capture=True)
+    packages = [m[1] for m in re.finditer(r"(?:^|\n)(.+?)\/", packages)]
+    if "libssl3" in packages:
+        CMAKE_FLAGS.append("-DWITH_OPENSSL3=TRUE")
+
+    print("Compiling (may take a while)")
+    run("cmake", "-DCMAKE_INSTALL_PREFIX=../install", *CMAKE_FLAGS, "../root",
+        cwd="./build")
+    run("cmake", "--build", ".", "--target", "install", f"-j{args.j:d}",
+        cwd="./build")
+
+    print("Done, remember to add a line like this to your .bashrc/.zshrc")
+    print(f"alias sroot='source {args.install_dir}/install/bin/thisroot.sh'")
